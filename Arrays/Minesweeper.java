c92ef24fb170be179d33a68ad62a4f99c4a08e74
Bhavesh Gadag <bhaveshgadag46@gmail.com>
2026-05-07 21:28:14 +0530

    finished minesweeper board

--- Arrays/Minesweeper.java
+++ Arrays/Minesweeper.java
@@ -5,65 +5,64 @@ public class Minesweeper {
         n = Integer.parseInt(args[1]);
         k = Integer.parseInt(args[2]);
 
-        int[] mines = new int[k];
-        for (int i = 0; i < k; i++) {
-            System.out.println("Adding for "+i);
-            boolean flag1 = false;
-            System.out.println("flag1 - "+flag1);
-            while (!flag1) {
-                System.out.println("entering while");
-                int position = (int) (Math.random() * (m * n)) + 1;
-                System.out.println("rindex "+position);
-                boolean flag = false;
-                for (int j = 0; j < k; j++){
-                    System.out.println("checking "+position+" in mines");
-                    if (mines[j] == position){
-                        flag = true;
-                        System.out.println("found in mines. changing flag to true");
-                        break;
+        if (k < (m * n)) {
+            int[][] board = new int[m][n];
+
+            for (int i = 0; i < k; ) {
+                int r = (int) (Math.random() * m);
+                int c = (int) (Math.random() * n);
+
+                if (board[r][c] != -1) {
+                    board[r][c] = -1;
+                    i++;
+                }
+            }
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (board[i][j] == -1) {
+                        if ((j - 1) >= 0 && board[i][j - 1] != -1) {
+                            board[i][j - 1]++;
+                        }
+                        if ((j + 1) < n && board[i][j + 1] != -1) {
+                            board[i][j + 1]++;
+                        }
+                        if ((i - 1) >= 0 && board[i - 1][j] != -1) {
+                            board[i - 1][j]++;
+                        }
+                        if ((i + 1) < m && board[i + 1][j] != -1) {
+                            board[i + 1][j]++;
+                        }
+                        if ((i - 1) >= 0 && (j - 1) >= 0 && board[i - 1][j - 1] != -1) {
+                            board[i - 1][j - 1]++;
+                        }
+                        if ((i - 1) >= 0 && (j + 1) < n && board[i - 1][j + 1] != -1) {
+                            board[i - 1][j + 1]++;
+                        }
+                        if ((i + 1) < m && (j - 1) >= 0 && board[i + 1][j - 1] != -1) {
+                            board[i + 1][j - 1]++;
+                        }
+                        if ((i + 1) < m && (j + 1) < n && board[i + 1][j + 1] != -1) {
+                            board[i + 1][j + 1]++;
+                        }
                     }
                 }
-                if (!flag){
-                    System.out.println("not found in mines, adding "+position+" in "+i);
+            }
 
-                    mines[i] = position;
-                    System.out.println("print mines");
-                    for (int t = 0; t < mines.length; t++) {
-                        System.out.print(mines[t] + "  ");
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (board[i][j] == -1) {
+                        System.out.print("*  ");
+                    } else {
+                        System.out.print(board[i][j] + "  ");
                     }
-                    System.out.println();
-                    System.out.println("flag in if - "+flag);
-                    break;
                 }
-                System.out.println("flag - "+flag);
+                System.out.println();
             }
         }
-
-        int[] board_seq = new int[m * n];
-        for (int i = 0; i < mines.length; i++) {
-            board_seq[mines[i]] = -1;
-            if ((mines[i] -1) >= 0 && board_seq[mines[i]-1] != -1)
-                board_seq[mines[i]-1]++;
-            if ((mines[i] +1) <= board_seq.length && board_seq[mines[i]+1] != -1)
-                board_seq[mines[i]+1]++;
+        else{
+            System.out.print("k cant be more than m*n");
         }
-
-//        for (int i = 0; i < board_seq.length; i++){
-//
-//        }
-
-        for (int t = 0; t < board_seq.length; t++) {
-            System.out.print(board_seq[t] + "  ");
-        }
-
-        int[][] board = new int[m][n];
-        System.out.println();
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < m; j++){
-                System.out.print(board[i][j]+"  ");
-            }
-            System.out.println();
-        }
-
     }
 }
