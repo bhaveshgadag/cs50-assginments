791455069c70d36e7e94009f4f89566f4ebb7aee
Bhavesh Gadag <bhaveshgadag46@gmail.com>
2026-05-08 20:00:02 +0530

    changed array value logic

--- Arrays/ThueMorse.java
+++ Arrays/ThueMorse.java
@@ -1,13 +1,10 @@
 public class ThueMorse {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int m = n * n;
-        char[][] a = new char[n][n];
-
         int[] t1 = new int[1];
         t1[0] = 0;
 
-        while (t1.length < (m)) {
+        while (t1.length < (n)) {
             int[] t2 = new int[t1.length];
             for (int i = 0; i < t1.length; i++) {
                 if (t1[i] == 0)
@@ -32,14 +29,16 @@ public class ThueMorse {
             t1 = c;
         }
 
-        int c = 0;
+        char[][] a = new char[n][n];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (t1[c] == 0)
+                if (t1[i] == t1[j]) {
                     a[i][j] = '+';
-                else
+                }
+                else {
                     a[i][j] = '-';
-                c++;
+                }
             }
         }
 
